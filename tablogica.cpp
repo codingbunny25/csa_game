52727d1dae5031c9252a7452e17f419f5ec90f65
gguiaraujo <guilherme30072011@gmail.com>
2026-06-03 20:04:00 +0100

    nome

--- tablogica.cpp
+++ tablogica.cpp
@@ -17,6 +17,30 @@ struct Carta {
 
 }
 
+
+Carta mineiro {   
+    3, //dano
+    7 //vida
+    0, //cura
+    'p', //classe
+    'g' //atributo
+
+}Carta Bobo {   
+    0, //dano
+    7 //vida
+    0, //cura
+    'p', //classe
+    't' //atributo
+}
+Carta Mulher que fala com espíritos {   
+    4, //dano
+    8 //vida
+    0, //cura
+    'e', //classe
+    'g' //atributo
+
+}
+
 //no mapa os quadrados têm cores ->   nada - 1 ; verde - 2 ; laranja - 3; azul - 4; amarelo - 5; vermelho - 6;portal - 7;
 
 int mapa [] = {1,2,1,2,1,1,2,1,1,4,1,2,3,1,4,6,1,2,1,2,1,1,2,1,1,1,1,4,1,6,1,3,1,1,2,3,2,1,1,4,1,2,6,1,3,1,2,1,5,1,4,1,6,5,4,7};
