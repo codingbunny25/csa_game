7829840c8ece752b7fcff92d0b542bbcb9a3732c
codingbunny25 <kito.csa@gmail.com>
2026-06-04 15:48:54 +0100

    baralhar, makePilha

--- tablogica.cpp
+++ tablogica.cpp
@@ -7,10 +7,12 @@
 
 
 
-#define max_verde 25
+#define max_verde 9
 #define max_azul 25
 #define max_vermelho 25
 #define max_amarelo 25
+
+
 #define max_baralho 31
 
 // typedefs, structs , mapa 
@@ -37,50 +39,23 @@ struct Carta {
 
 
 
-Carta mineiro {   
-    "Mineiro",
-    3, //dano
-    7, //vida
-    0, //cura
-    0,//raridade
-    'p', //classe
-    'g' //atributo
-
-};
-Carta bobo {  
-    "Bobo", 
-    0, //dano
-    7, //vida
-    0, //cura
-    0,raridade
-    'p', //classe
-    't' //atributo
-};
-Carta mulher_espiritos {   
-    "Mulher que fala \ncom espíritos",
-    4, //dano
-    8, //vida
-    0, //cura
-    0,//raridade
-    'e', //classe
-    'g' //atributo
-
-};
-
 struct Pilhas{
 
     Carta verde [max_verde] ;
     int spverde;
     Carta azul [max_azul] ;
     int spazul;
-    Carta vermelha [max_azul] ;
+    Carta vermelha [max_vermelho] ;
     int spvermelha;
     Carta amarela [max_amarelo] ;
     int spamarela;
 };
 
 
-
+struct Jogador {
+    Carta cartas[31];
+    int posi;
+};
 
 
 //no mapa os quadrados têm cores ->   nada - 1 ; verde - 2 ; laranja - 3; azul - 4; amarelo - 5; vermelho - 6;portal - 7;
@@ -105,8 +80,8 @@ Carta baralho [max_baralho];
 int dado () {
 
     std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> die(1,6);
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> die(1,6);
 
 
     return die(gen);
@@ -122,7 +97,7 @@ void baralhar (Carta baralho []){
         
         std::uniform_int_distribution<> die(0,i);
 
-        int r = max_baralho - die(gen);
+        int r = die(gen);
 
         Carta temp = baralho[i];
         baralho [i] = baralho [r];
@@ -135,22 +110,104 @@ void baralhar (Carta baralho []){
 
 
 
-
-
 Pilhas makePilhas(Carta baralho []){
 
-    Pilhas p ;
+    Pilhas  p ;
 
 
+    p.spamarela=0;
+    p.spazul =0;
+    p.spverde =0;
+    p.spvermelha =0;
+
+
+
+    baralhar(baralho);
+
+
+
+
+    //escreve as cartas nos seus sítios nos arrays dentro da pilha
     for(int i =0;i<max_baralho;i++){
+
+        if(baralho[i].raridade == 0){
+            p.verde[p.spverde] = baralho[i]; 
+            p.spverde ++;
+        }
+
+        if(baralho[i].raridade == 1){
+            p.azul[p.spazul] = baralho[i]; 
+            p.spazul ++;
+        }
+
+        if(baralho[i].raridade == 2){
+            p.vermelha[p.spvermelha] = baralho[i]; 
+            p.spvermelha ++;
+        }
+            if(baralho[i].raridade == 3){
+            p.amarela[p.spamarela] = baralho[i]; 
+            p.spamarela ++;
+        }
 
     }
 
-
-
-
+    //assim os stackpointers apontam para a primeira carta da pilha
+    p.spamarela--;
+    p.spvermelha--;
+    p.spverde--;
+    p.spazul--;
 
     return p;
 
 }
 
+//no mapa os quadrados têm cores ->   nada - 1 ; verde - 2 ; laranja - 3; azul - 4; amarelo - 5; vermelho - 6;portal - 7;
+
+
+void auxTileVerde(Jogador j){
+
+}
+
+void auxTileLaranja(Jogador j){
+    
+}
+
+
+
+void auxTileAzul    (Jogador j){
+
+}
+void auxTileAmarelo (Jogador j){
+
+}
+void auxTileVermelho(Jogador j){
+
+} 
+
+
+void gereTiles (Jogador j, int tile){
+
+    switch (tile){
+
+        case 2:
+            auxTileVerde(j);
+            break;
+
+        case 3:
+            auxTileLaranja(j);
+            break;
+        
+        case 4 :
+            auxTileAzul(j);
+            break;
+        case 5:
+            auxTileAmarelo(j);
+            break;
+        case 6:
+            auxTileVermelho(j);    
+        
+        default : 
+        break;
+        
+    }
+}
