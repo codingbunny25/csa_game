da55823636c229eb397c531fa6b40d7c2726ed92
codingbunny25 <kito.csa@gmail.com>
2026-06-04 14:30:01 +0100

    cartas

--- tablogica.cpp
+++ tablogica.cpp
@@ -8,38 +8,46 @@ typedef char atributo;//dano em área->d, atirador->a, alvo->t, guerreiro->g, su
 
 
 struct Carta {
+    char nome [128];
     int dano;
     int vida;
     int cura;
+    int raridade;//0
     classe cl;
-    atributo at;
+    atributo at1;
+    atributo at2;
 
+    
 
-}
+};
 
 
 Carta mineiro {   
+    "Mineiro",
     3, //dano
-    7 //vida
+    7, //vida
     0, //cura
     'p', //classe
     'g' //atributo
 
-}Carta bobo {   
+};
+Carta bobo {  
+    "Bobo", 
     0, //dano
-    7 //vida
+    7, //vida
     0, //cura
     'p', //classe
     't' //atributo
-}
+};
 Carta mulher_espiritos {   
+    "Mulher que fala \ncom espíritos",
     4, //dano
-    8 //vida
+    8, //vida
     0, //cura
     'e', //classe
     'g' //atributo
 
-}
+};
 
 
 
@@ -51,5 +59,7 @@ int mapa [] = {1,2,1,2,1,1,2,1,1,4,1,2,3,1,4,6,1,2,1,2,1,1,2,1,1,1,1,4,1,6,1,3,1
 
 
 int dado () {
+
+    
 
 }
